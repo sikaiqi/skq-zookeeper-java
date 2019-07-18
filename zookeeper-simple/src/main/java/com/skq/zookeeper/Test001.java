cd0779b3438d2a169f741d2c8ec306ecec689e44
sikaiqi <sikaiqi1992@163.com>
2019-07-18 21:47:39 +0800

    practice

--- zookeeper-simple/src/main/java/com/skq/zookeeper/Test001.java
+++ zookeeper-simple/src/main/java/com/skq/zookeeper/Test001.java
@@ -6,44 +6,45 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.Watcher.Event.EventType;
 import org.apache.zookeeper.Watcher.Event.KeeperState;
-import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * Created by skq on 2019/7/9.
+ * Created by Administrator on 2019/7/18 0018.
  */
 public class Test001 {
-    //连接地址
-    private static final String ADDRES = "127.0.0.1:2181";
-    //session 会话
-    private static final int SESSION_OUTTIME = 2000;
+    //zk地址
+    private static final String SERVERADDRESS = "127.0.0.1";
+    private static final int SESSIONTIMEOUT = 2000;
     //信号量,阻塞程序执行,用户等待zookeeper连接成功,发送成功信号，
-    private static final CountDownLatch countDownLatch = new CountDownLatch(1);
+    private static final CountDownLatch COUNT_DOWN_LATCH = new CountDownLatch(1);
+
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-        ZooKeeper zk = new ZooKeeper(ADDRES, SESSION_OUTTIME, new Watcher() {
-            public void process(WatchedEvent event) {
-                // 获取事件状态
-                Event.KeeperState keeperState = event.getState();
-                // 获取事件类型
-                EventType eventType = event.getType();
-                if (KeeperState.SyncConnected == keeperState) {
-                    if (EventType.None == eventType) {
-                        countDownLatch.countDown();
-                        System.out.println("zk 启动连接...");
+        ZooKeeper zk = new ZooKeeper(SERVERADDRESS, SESSIONTIMEOUT, new Watcher() {
+            public void process(WatchedEvent watchedEvent) {
+                //获取事件状态
+                KeeperState state = watchedEvent.getState();
+                //获取事件类型
+                EventType type = watchedEvent.getType();
+
+                if (KeeperState.SyncConnected.equals(state)) {
+                    if (EventType.None.equals(type)) {
+                        COUNT_DOWN_LATCH.countDown();
+                        System.out.println("zk server 连接启动成功！");
                     }
                 }
+
             }
         });
-        // 进行阻塞
-        countDownLatch.await();
-        String result = zk.create("/itmayeidu_Lasting", "Lasting".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.PERSISTENT);
-        System.out.println(result);
+        System.out.println("zk server  等待连接。。。。");
+        //zk 未连接上服务时需要阻塞。。。
+        COUNT_DOWN_LATCH.await();
+
+        String node = zk.create("/skq", "first zk node".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println(node);
         zk.close();
     }
-
 }
-
